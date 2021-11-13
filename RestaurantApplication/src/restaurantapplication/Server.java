2176fbc648a1d7fe74ed62f5ca16a6e1465fbbb1
Anthony Howell <anthonyamh@gmail.com>
2021-11-13 11:41:43 -0600

    Completed methods

--- RestaurantApplication/src/restaurantapplication/Server.java
+++ RestaurantApplication/src/restaurantapplication/Server.java
@@ -1,31 +1,35 @@
 package restaurantapplication;
 
 public class Server extends Employee {
-	
+
     /* CONSTRUCTORS */
 
 	public Server(int employeeId, String employeeFirstname, String employeeLastname, String employeePosition, String employeePassword) {
-		
+
 		super(employeeId, employeeFirstname, employeeLastname, employeePosition, employeePassword);
 
 	}
-	
+
 	/* CLASS METHODS */
 
 	//Adds an order entry to the RestaurantModel's ordersMap
-	
+
 	public void addOrder(Order order, RestaurantModel model) {
 
+		model.getOrdersMap().put(order.getNumber(), order);
 		
-		
+		model.updateOrders();
+
 	}
-	
+
 	//Removes an order entry from the RestaurantModel's ordersMap
-	
+
 	public void removeOrder(Order order, RestaurantModel model) {
-		
-		
-		
+
+		model.getOrdersMap().remove(order.getNumber());
+
+		model.updateOrders();
+
 	}
-	
+
 }
